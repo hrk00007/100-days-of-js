a37eb8867f6c280dbccec8438bda1e57fe427215
hrk00007 <hritikmanbattulwar@gmail.com>
2022-10-27 21:51:04 +0530

    Day 1 Creating a simple Counter app

---
+++ Day 1/script.js
@@ -0,0 +1,51 @@
+/*
+Method 1
+
+const add = document.querySelector(".add");
+const reset = document.querySelector(".reset");
+const sub = document.querySelector(".substract");
+const count = document.querySelector(".count");
+
+add.addEventListener("click", () => {
+    count.innerHTML++;
+});
+
+sub.addEventListener("click", () => {
+    count.innerHTML--;
+});
+
+reset.addEventListener("click", () => {
+    count.innerHTML = 0;
+});
+
+*/
+
+//Method 2
+const count = document.querySelector(".count");
+const buttons=document.querySelector(".buttons");
+
+buttons.addEventListener("click",(e)=>{
+        if(e.target.classList.contains("add")){
+            count.innerHTML++;
+        }
+        else if(e.target.classList.contains("substract")){
+            count.innerHTML--;
+        }
+        else{
+            count.innerHTML=0;
+        }
+        setColor();
+});
+
+function setColor(){
+    if(count.innerHTML==0){
+        count.style.color="yellow";
+    }
+    else  if(count.innerHTML>0){
+        count.style.color="green";
+    }
+    else{
+        count.style.color="red";
+        
+    }
+}
