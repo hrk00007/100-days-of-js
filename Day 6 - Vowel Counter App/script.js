89eb547a006cc8d2bac4e604675f102a847cdfeb
hrk00007 <hritikmanbattulwar@gmail.com>
2022-11-03 22:47:27 +0530

    Day 6 In this project I created Vowel counter app which will return number of vowel in given String

---
+++ Day 6 - Vowel Counter App/script.js
@@ -0,0 +1,16 @@
+const word=document.querySelector(".input-text");
+const btn=document.querySelector(".btn");
+const result=document.querySelector(".result");
+
+btn.addEventListener("click",()=>{
+    var vowel_count=0;
+    var str=word.value;
+    for(var ch=0;ch<str.length;ch++){
+        if(str[ch]=='a' || str[ch]=='e' ||str[ch]=='i' ||str[ch]=='o' ||str[ch]=='u' ||
+           str[ch]=='A' || str[ch]=='E' ||str[ch]=='I' ||str[ch]=='O' ||str[ch]=='U' ){
+            vowel_count++;
+        }
+    }
+    result.innerHTML="<h3>The count of vowel is "+ `${vowel_count}`+" </h3>";
+  
+})
